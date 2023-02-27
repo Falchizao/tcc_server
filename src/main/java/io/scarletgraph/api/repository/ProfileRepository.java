a9cba71b962d318771f61b761c1d9ebcf394d535
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-02-26 23:02:13 -0300

    feat: profile services

---
+++ src/main/java/io/scarletgraph/api/repository/ProfileRepository.java
@@ -0,0 +1,12 @@
+package io.scarletgraph.api.repository;
+
+import io.scarletgraph.api.domain.Profile;
+import io.scarletgraph.api.generic.IRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.Optional;
+
+public interface ProfileRepository extends IRepository<Profile> {
+
+    @Query(nativeQuery = true, value = "SELECT profile.* FROM profile INNER JOIN tb_user us on obj.id = us.profile_id where us.id=:id")
+    Optional<Profile> findProfileByUserId(Long id);
+}
---
+++ src/main/java/io/scarletgraph/api/service/CRUD/ProfileCRUDService.java
@@ -0,0 +1,40 @@
+package io.scarletgraph.api.service.CRUD;
+
+import io.scarletgraph.api.domain.Profile;
+import io.scarletgraph.api.domain.User;
+import io.scarletgraph.api.handler.modelException.ResourceNotFound;
+import io.scarletgraph.api.repository.ProfileRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Slf4j
+public class ProfileCRUDService {
+
+    private final ProfileRepository profileRespository;
+
+
+    public ProfileCRUDService(ProfileRepository profileRespository) {
+        this.profileRespository = profileRespository;
+    }
+
+
+    public Profile getProfileByUser(Long id) {
+        try{
+            Optional<Profile> profile = profileRespository.findProfileByUserId(id);
+            if(profile.isEmpty()){
+                throw new ResourceNotFound("Profile not found by user!");
+            }
+            return profile.get();
+        } catch (Exception e){
+            throw new ResourceNotFound("Error fetching profile!");
+        }
+    }
+
+    public void add (Profile profile){
+        log.info("Saving profile....");
+        profileRespository.save(profile);
+    }
+}
