345c460532b6f45d0e6240500cdf5c2b43d504c8
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-02-26 23:00:16 -0300

    refactor: relation profile x images

--- src/main/java/io/scarletgraph/api/domain/Profile.java
+++ src/main/java/io/scarletgraph/api/domain/Profile.java
@@ -1,23 +1,37 @@
 package io.scarletgraph.api.domain;
 
-import io.scarletgraph.api.domain.social.Picture;
 import io.scarletgraph.api.domain.social.Skill;
 import io.scarletgraph.api.generic.IModel;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.util.List;
 
+@Entity
 public class Profile extends IModel {
 
     @Getter
     @Setter
-    private Picture picture;
+    @Lob
+    @Column(name="profile_banner")
+    private byte[] profile_banner;
 
     @Getter
     @Setter
-    @OneToMany
+    @Lob
+    @Column(name="profile_picture")
+    private byte[] profile_picture;
+
+    @Setter
+    @OneToOne(mappedBy = "profile")
+    private User user;
+
+    @Getter
+    @Setter
+    @OneToMany(mappedBy = "profile",
+            orphanRemoval = true,
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
     private List<Skill> skills;
 
     @Getter
