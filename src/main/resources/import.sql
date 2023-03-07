deef5d210cec864eeab271ba079e54bf7697126c
Marcelo Falchi <marcelonavarro11md@gmail.com>
2023-03-07 13:31:27 -0300

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,23 @@
 INSERT INTO profile(description) VALUES ('boilerplate')
 INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('marcelo@gmail.com', 'Marcelo', 'Falchi', 'a', 1, 'falchi', 1)
+
+INSERT INTO profile(description) VALUES ('boilerplate')
+INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('johndoe@example.com', 'John', 'Doe', '$2a$10$Tb85oR8uoEa1e36EGhCJ6OMyMQs9EdW6DNv3J15hERbTgT8RnlW1a', 1, 'johndoe', 2);
+
+INSERT INTO profile(description) VALUES ('boilerplate')
+INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('janesmith@example.com', 'Jane', 'Smith', '$2a$10$7grx.5JLnPIVTOLuOYIzfu7XJhvKdUhVIlOFTBco6rhVgPQJ0cHqK', 1, 'janesmith', 3);
+
+INSERT INTO profile(description) VALUES ('boilerplate')
+INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('robertjones@example.com', 'Robert', 'Jones', '$2a$10$W2u3il.3MEU6KzHMuUnsW.X9k43dHKPiA6fZT6TJW8xw26B1AlY0a', 1, 'robertjones', 4);
+
+INSERT INTO profile(description) VALUES ('boilerplate')
+INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('sarahlee@example.com', 'Sarah', 'Lee', '$2a$10$w4bUHhyjA10.6UVs9Bq3SO6jKm6TmT3q9XZ.vtH4a4zKgRlCXxoE.', 'user', 2, 5);
+
+INSERT INTO profile(description) VALUES ('boilerplate')
+INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('michaelbrown@example.com', 'Michael', 'Brown', '$2a$10$iCwzF50mt0jKzwfsLGxS0.4ukdK2N3X9gxIKDRtET4fH/0/XRzgmy', 2, 'michaelbrown', 6);
+
+INSERT INTO profile(description) VALUES ('boilerplate')
+INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('emilynguyen@example.com', 'Emily', 'Nguyen', '$2a$10$JY8llvzU91AOSpVl/DHMH.NXlrgcFlKbsh27rkQzZ38fYdL26jF8S', 2, 'emilynguyen', 7);
+
+INSERT INTO profile(description) VALUES ('boilerplate')
+INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('davidsmith@example.com', 'David', 'Smith', '$2a$10$hE2hBzUNOzN41yf25hJfAOE7i8Mk5p5ZK/6/hzUwOY8HzGJPPKjy2', 2, 'davidsmith', 8);
