abcd9264b6149fe084762ba10d7f5ae430f4ed7d
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-04-09 23:31:44 -0300

    refactor: import.sql to auto load profiles

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,8 +2,8 @@ insert into authority (authority) values ('ROLE_ADMIN');
 insert into authority (authority) values ('ROLE_CANDIDATE');
 insert into authority (authority) values ('ROLE_COMPANY');
 
-INSERT INTO profile(description) VALUES ('boilerplate')
-INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('marcelo@gmail.com', 'Marcelo', 'Falchi', 'a', 1, 'falchi', 1)
+INSERT INTO profile(description) VALUES ('Me chamo Marcelo Falchi, sou o criador deste app, busco sempre novos conhecimentos e me aprofundar em novas tecnologias')
+INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('marcelonavarro@gmail.com', 'Marcelo', 'Falchi', '$2a$10$lLPJdpiaK3AHpfklvkIa1.O93tRYQCVYbtlA3CUNdXpQNY8PGJnti', 1, 'falchi', 1)
 
 INSERT INTO profile(description) VALUES ('boilerplate')
 INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('johndoe@example.com', 'Lary', 'Doe', '$2a$10$Tb85oR8uoEa1e36EGhCJ6OMyMQs9EdW6DNv3J15hERbTgT8RnlW1a', 1, 'larydoe', 2);
@@ -23,21 +23,36 @@ INSERT INTO TB_USER (email, first_name, last_name, password, type, username, pro
 INSERT INTO profile(description) VALUES ('boilerplate')
 INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('emilynguyen@example.com', 'Emily', 'Nguyen', '$2a$10$JY8llvzU91AOSpVl/DHMH.NXlrgcFlKbsh27rkQzZ38fYdL26jF8S', 2, 'emilynguyen', 7);
 
-INSERT INTO profile(description) VALUES ('boilerplate')
+INSERT INTO profile(description) VALUES ('')
 INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('davidsmith@example.com', 'Samara', 'Smith', '$2a$10$hE2hBzUNOzN41yf25hJfAOE7i8Mk5p5ZK/6/hzUwOY8HzGJPPKjy2', 2, 'samarasmith', 8);
 
-INSERT into offer (title, content, created_date, hours, salary, tb_user) VALUES ('Pessoa Programadora Delphi Pleno', 'Atenciosa, prestativa e gostar de trabalhar', now(), 8.48, 1800, 1);
-INSERT into offer (title, content, created_date, hours, salary, tb_user) VALUES ('Pessoa Programadora Java Senior', 'Buscamos pessoas com facilidade de aprendizado', now(), 8, 5000, 2);
-INSERT into offer (title, content, created_date, hours, salary, tb_user) VALUES ('Pessoa Programadora Node JS Pleno', 'Deve-se conhecer bem node js', now(), 8, 4200, 3);
+INSERT INTO profile(description) VALUES ('Me chamo ronald, sou recrutador da empresa XX, fique por dentro de novas vagas clicando no botao de seguir')
+INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('ronald@example.com', 'Ronald', 'Black', '$2a$10$hE2hBzUNOzN41yf25hJfAOE7i8Mk5p5ZK/6/hzUwOY8HzGJPPKjy2', 2, 'ronaldblack', 9);
+
+INSERT INTO profile(description) VALUES ('Me chamo vitor, adoro ler e comentar sobre programacao, se for de seu interesse, me siga para mais informacoes')
+INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('victor@example.com', 'Victor', 'Santana', '$2a$10$hE2hBzUNOzN41yf25hJfAOE7i8Mk5p5ZK/6/hzUwOY8HzGJPPKjy2', 2, 'victorsantana', 10);
+
+INSERT INTO profile(description) VALUES ('Professor Universitario, programador fullstack e amo jogos virtuais')
+INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('pegorini@example.com', 'Vinicius', 'Pegorini', '$2a$10$hE2hBzUNOzN41yf25hJfAOE7i8Mk5p5ZK/6/hzUwOY8HzGJPPKjy2', 2, 'vinipegorini', 11);
+
+INSERT INTO profile(description) VALUES ('Professora Universitaria, com muita vontade de ensinar!')
+INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('soelaine@example.com', 'Gabrielle', 'Soelaine', '$2a$10$hE2hBzUNOzN41yf25hJfAOE7i8Mk5p5ZK/6/hzUwOY8HzGJPPKjy2', 2, 'soelaine', 12);
+
+INSERT INTO profile(description) VALUES ('Programador C#, amo ensinar novas pessoas!')
+INSERT INTO TB_USER (email, first_name, last_name, password, type, username, profile_id) VALUES ('bueno@example.com', 'Victor', 'Bueno', '$2a$10$hE2hBzUNOzN41yf25hJfAOE7i8Mk5p5ZK/6/hzUwOY8HzGJPPKjy2', 2, 'victorbueno', 13);
+
+INSERT into offer (title, content, created_date, hours, salary, tb_user) VALUES ('Programadora Delphi', 'Atenciosa, prestativa e gostar de trabalhar', now(), 8.48, 1800, 1);
+INSERT into offer (title, content, created_date, hours, salary, tb_user) VALUES ('Programadora Java', 'Buscamos pessoas com facilidade de aprendizado', now(), 8, 5000, 2);
+INSERT into offer (title, content, created_date, hours, salary, tb_user) VALUES ('Programadora Node JS', 'Deve-se conhecer bem node js', now(), 8, 4200, 3);
 INSERT into offer (title, content, created_date, hours, salary, tb_user) VALUES ('Arquiteto Java', 'Microsservices e Kafka', now(), 8, 13500, 4);
 INSERT into offer (title, content, created_date, hours, salary, tb_user) VALUES ('Estagio PHP', 'Composer e Fullstack', now(), 6, 1500, 5);
-INSERT into offer (title, content, created_date, hours, salary, tb_user) VALUES ('Programadora Ruby on Rails', '+ de 3 anos de experiencia', now(), 8, 3200, 6);
-INSERT into offer (title, content, created_date, hours, salary, tb_user) VALUES ('Pessoa Programadora Flutter', 'Getx e RiverPod', now(), 8, 4300, 7);
+INSERT into offer (title, content, created_date, hours, salary, tb_user) VALUES ('Programadora Ruby', '+ de 3 anos de experiencia', now(), 8, 3200, 6);
+INSERT into offer (title, content, created_date, hours, salary, tb_user) VALUES ('Programadora Flutter', 'Getx e RiverPod', now(), 8, 4300, 7);
 
 INSERT INTO post (content, created_date, tb_user_id) values ('Adoro essa plataforma!', now(), 1);
-INSERT INTO post (content, created_date, tb_user_id) values ('Aff, php é muito ruim', now(), 2);
-INSERT INTO post (content, created_date, tb_user_id) values ('Pessoal, minha empresas está contratando! Vagas para pleno e senior hehe', now(), 3);
+INSERT INTO post (content, created_date, tb_user_id) values ('Aff, php ficou muito ruim', now(), 2);
+INSERT INTO post (content, created_date, tb_user_id) values ('Pessoal, minha empresas abriu algumas vagas! Tanto para pleno e senior hehe', now(), 3);
 INSERT INTO post (content, created_date, tb_user_id) values ('Virei Tech Lead, muito feliz', now(), 4);
 INSERT INTO post (content, created_date, tb_user_id) values ('Consegui meu primeiro emprego como programadora aqui, adorei!!', now(), 5);
-INSERT INTO post (content, created_date, tb_user_id) values ('Que bacana esse sistema de adicionar as conexões', now(), 6);
+INSERT INTO post (content, created_date, tb_user_id) values ('Que bacana esse sistema de seguir as pessoas', now(), 6);
 INSERT INTO post (content, created_date, tb_user_id) values ('Adorei esse sistema de chat!', now(), 7);
