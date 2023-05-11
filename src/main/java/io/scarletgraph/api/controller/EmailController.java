98c124812cabd2a5cd4da0448c746298c82d9a52
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-05-10 22:24:59 -0300

    delete offer card action

--- src/main/java/io/scarletgraph/api/controller/EmailController.java
+++ src/main/java/io/scarletgraph/api/controller/EmailController.java
@@ -1,5 +1,7 @@
 package io.scarletgraph.api.controller;
 
+import io.scarletgraph.api.dto.emailDTO.EmailSenderRequest;
+import io.scarletgraph.api.service.CRUD.OfferCRUDService;
 import io.scarletgraph.api.service.EmailService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -11,17 +13,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/email")
 public class EmailController {
     private final EmailService emailService;
+    private final OfferCRUDService offerCRUDService;
 
-    public EmailController (EmailService emailService) {
+    public EmailController (EmailService emailService, OfferCRUDService offerCRUDService) {
         this.emailService = emailService;
+        this.offerCRUDService = offerCRUDService;
     }
 
-    @PostMapping("/sendEmail/{candidate}")
-    public ResponseEntity<?> sendEmail(@PathVariable String candidate, @RequestBody Long offerID) {
+
+    @PostMapping("/sendEmail")
+    public ResponseEntity<?> sendEmail(@RequestBody EmailSenderRequest emailRequest) {
 
         try {
-            emailService.sendEmail(candidate, offerID);
+            emailService.sendEmail(emailRequest.getUsername(), emailRequest.getOffer_id());
             log.info("Email sent to candidate!");
+
+            log.info("removing the offer..");
+            offerCRUDService.delete(emailRequest.getOffer_id());
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
